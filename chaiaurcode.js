073440808b20f439da7734aae80c8d36a5f39a1a
VansitaYGajjar <vansita.g@empiricinfotech.com>
2024-08-12 11:03:27 +0530

    first commit

---
+++ chaiaurcode.js
@@ -0,0 +1,34 @@
+// Generate the random color
+
+const randomColor = function () {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+  
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  
+  let intervalId;
+  
+  const startChangingColor = function () {
+    if (!intervalId) {
+      intervalId = setInterval(changeBGColor, 1000);
+    }
+  
+    function changeBGColor() {
+      document.body.style.backgroundColor = randomColor();
+    }
+    console.log('Started');
+  };
+  
+  const stopChangingColor = function () {
+    clearInterval(intervalId);
+    intervalId = null;
+    console.log('Stopped');
+  };
+  
+  document.querySelector('#start').addEventListener('click', startChangingColor);
+  document.querySelector('#stop').addEventListener('click', stopChangingColor);
+  
